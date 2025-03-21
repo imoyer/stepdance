3a49c55eb3503f413f989c2a2fc67f6ac46f6828
Ilan Moyer <imoyer@mit.edu>
2025-03-20 20:20:13 -0400

    moved core code into a seperate library folder

---
+++ lib/stepdance.hpp
@@ -0,0 +1,13 @@
+#include "analog_in.hpp"
+#include "channels.hpp"
+#include "core.hpp"
+#include "input_ports.hpp"
+#include "interface_ebb.hpp"
+#include "interpolators.hpp"
+#include "kinematics.hpp"
+#include "output_ports.hpp"
+
+
+
+
+
