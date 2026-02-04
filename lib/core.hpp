832d16960bbb676abce617fedf2240306d7229df
em-yu <ermahler.yu@gmail.com>
2026-02-04 08:20:24 -0800

    Propose increasing max number of plugins

--- lib/core.hpp
+++ lib/core.hpp
@@ -84,10 +84,10 @@ static volatile uint32_t stepdance_interrupt_entry_cycle_count = 0; //stores the
 //
 // This provides a common interface for any filters, synthesizers, kinematics, etc that need access to the core frame.
 #define MAX_NUM_INPUT_PORT_FRAME_PLUGINS   10 //plugins that execute at the start of the frame. This is reserved for input ports
-#define MAX_NUM_PRE_CHANNEL_FRAME_PLUGINS   10 //plugins that execute in the frame, before the channels are evaluated
+#define MAX_NUM_PRE_CHANNEL_FRAME_PLUGINS   20 //plugins that execute in the frame, before the channels are evaluated
 #define MAX_NUM_POST_CHANNEL_FRAME_PLUGINS  10 //plugins that execute in the frame, after the channels are evaluated
 #define MAX_NUM_KILOHERTZ_PLUGINS 10 //plugins that execute at a 1khz rate, independent of the frame, and with a lower priority
-#define MAX_NUM_LOOP_PLUGINS 10 //plugins that execute in the main loop.
+#define MAX_NUM_LOOP_PLUGINS 20 //plugins that execute in the main loop.
 /** \cond */
 /**
  * Plugin Base Class will be hidden from Doxygen documentation.
