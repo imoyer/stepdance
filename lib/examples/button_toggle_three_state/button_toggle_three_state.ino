0469b5621a437ff3737153be1f6bdab50f12bc9d
pixelmaid <jennifermaryjacobs@gmail.com>
2026-04-16 15:00:27 +0200

    added three state toggle, untested

--- lib/examples/button_toggle_three_state/button_toggle_three_state.ino
+++ lib/examples/button_toggle_three_state/button_toggle_three_state.ino
@@ -1,11 +1,6 @@
 /*
-AxiDraw Interface
 
-This extends the Step-A-Sketch demo with an InkScape interface that simulates the AxiDraw's
-EBB Control Board.
-
-Example project for the Stepdance control system.
-
+Three state toggle example 
 A part of the Mixing Metaphors Project
 
 // (c) 2026 Ilan Moyer, Jennifer Jacobs, Emilie Yu, Alejandro Aponte
@@ -14,14 +9,7 @@ A part of the Mixing Metaphors Project
 #define module_driver   // tells compiler we're using the Stepdance Driver Module PCB
                         // This configures pin assignments for the Teensy 4.1
 
-// Machine Selection
-//#define axidraw 
-#define pocket_plotter
-
-
 #include "stepdance.hpp"  // Import the stepdance library
-// -- Define Input Ports --
-InputPort input_a;
 
 // -- Define Output Ports --
 // Output ports generate step and direction electrical signals
@@ -41,9 +29,6 @@ Channel channel_a;  //AxiDraw "A" axis --> left motor motion
 Channel channel_b;  // AxiDraw "B" axis --> right motor motion
 Channel channel_z;  // AxiDraw "Z" axis --> pen up/down
 
-// -- AxiDraw Interface
-Eibotboard ebb_interface;
-
 // -- Define Kinematics --
 // Kinematics convert between two coordinate spaces.
 // We think in XY, but the axidraw moves in AB according to "CoreXY" (also "HBot") kinematics
@@ -55,18 +40,15 @@ KinematicsCoreXY axidraw_kinematics;
 Encoder encoder_1;  // left knob, controls horizontal
 Encoder encoder_2;  // right knob, controls vertical
 
-// -- Define Input Button --
+// -- Define Input Buttons --
 Button button_d1;
 Button button_d2;
 
 // -- Position Generator for Pen Up/Down --
 PositionGenerator position_gen;
 
-// -- 2D Scaling Filter to scale up/down output --//
-ScalingFilter2D scalingFilter2D;
-
-// -- Potentiometer to control scaling ratio -- //
-AnalogInput scaling_slider_a2;
+// -- 1D Wave Generator for Pen up down oscillation--
+WaveGenerator1D wave_gen;
 
 void setup() {
   // -- Configure and start the output ports --
@@ -80,35 +62,18 @@ void setup() {
   // -- Configure and start the channels --
   channel_a.begin(&output_a, SIGNAL_E); // Connects the channel to the "E" signal on "output_a".
                                         // We choose the "E" signal because it results in a step pulse of 7us,
-                                        // which is more than long enough for the driver IC
+                                        // which is more than long enough for the driver IC.
+  channel_a.set_ratio(25.4, 2032); // Sets the input/output transmission ratio for the channel.
+                                                // This provides a convenience of converting between input units and motor (micro)steps
+                                                // For the axidraw, 25.4mm == 2874 steps
   channel_a.invert_output();  // CALL THIS TO INVERT THE MOTOR DIRECTION IF NEEDED
+
   channel_b.begin(&output_b, SIGNAL_E);
+  channel_b.set_ratio(25.4, 2032);
   channel_b.invert_output();
 
-#ifdef axidraw
-  channel_a.set_ratio(25.4, 2032);
-  channel_b.set_ratio(25.4, 2032);
-#endif
-#ifdef pocket_plotter
-  channel_a.set_ratio(40, 3200); // Sets the input/output transmission ratio for the channel.
-  channel_b.set_ratio(40, 3200);
-#endif
-                                                // This provides a convenience of converting between input units and motor (micro)steps
-                                                // For the pocket plotter, 40mm == 3200 steps (1/16 microstepping)
-
   channel_z.begin(&output_c, SIGNAL_E); //servo motor, so we use a long pulse width
-  channel_z.set_ratio(1, 50); //straight step pass-thru.
-
-  // -- Configure and start the input port --
-  input_a.begin(INPUT_A);
-  input_a.output_x.set_ratio(0.01, 1); //1 step is 0.01mm
-  input_a.output_x.map(&axidraw_kinematics.input_x);
-
-  input_a.output_y.set_ratio(0.01, 1); //1 step is 0.01mm
-  input_a.output_y.map(&axidraw_kinematics.input_y);
-
-  input_a.output_z.set_ratio(0.01, 1); //1 step is 0.01mm
-  input_a.output_z.map(&channel_z.input_target_position);
+  channel_z.set_ratio(1, 1); //straight step pass-thru.
 
   // -- Configure and start the encoders --
   encoder_1.begin(ENCODER_1); // "ENCODER_1" specifies the physical port on the PCB
@@ -123,47 +88,33 @@ void setup() {
   encoder_2.invert();
   encoder_2.output.map(&axidraw_kinematics.input_y); // map the right encoder to the y axis input of the kinematics
 
-  // -- Configure and start EBB Interface --
-  ebb_interface.begin();
-  ebb_interface.output_x.map(&axidraw_kinematics.input_x);
-  ebb_interface.output_y.map(&axidraw_kinematics.input_y);
-  ebb_interface.output_z.map(&channel_z.input_target_position);
-
   // -- Configure and start the kinematics module --
   axidraw_kinematics.begin();
-  // -- Map the kinematics output to the scaling filter input --//
-  axidraw_kinematics.output_a.map(&scalingFilter2D.input_1);
-  axidraw_kinematics.output_b.map(&scalingFilter2D.input_2);
+  axidraw_kinematics.output_a.map(&channel_a.input_target_position);
+  axidraw_kinematics.output_b.map(&channel_b.input_target_position);
 
-  // -- Configure Buttons --
+
+  // -- Configure Pen Button --
   button_d1.begin(IO_D1, INPUT_PULLDOWN);
   button_d1.set_mode(BUTTON_MODE_TOGGLE);
-  button_d1.set_callback_on_press(&pen_up);
-  button_d1.set_callback_on_release(&pen_down);
+  button_d1.set_callback_on_press(&pen_down);
+  button_d1.set_callback_on_release(&pen_up);
 
+    // -- Configure 3 state toggle button --
   button_d2.begin(IO_D2, INPUT_PULLDOWN);
-  button_d2.set_mode(BUTTON_MODE_TOGGLE);
-  button_d2.set_callback_on_press(&motors_enable);
-  button_d2.set_callback_on_release(&motors_disable);
+  button_d2.set_mode(BUTTON_MODE_TOGGLE_THREE_STATE);
+  button_d2.set_callback_on_first_press(&generator_state_1);
+  button_d2.set_callback_on_second_press(&generator_state_2);
+  button_d2.set_callback_on_third_press(&generator_state_3);
 
   // -- Configure Position Generator --
   position_gen.output.map(&channel_z.input_target_position);
   position_gen.begin();
 
-// -- Configure the Scaling Filter -- //
-  scalingFilter2D.begin(); 
-  scalingFilter2D.set_ratio(0.5); // this will ultimately be set by the scaling slider below
-  scalingFilter2D.output_1.map(&channel_a.input_target_position); // map the outputs of the scaling filter to channel a and b inputs
-  scalingFilter2D.output_2.map(&channel_b.input_target_position);
 
-// -- Configure the scaling slider -- //
-  scaling_slider_a2.begin(IO_A2);
-  scaling_slider_a2.set_floor(0.5);
-  scaling_slider_a2.set_ceiling(2);
-  scaling_slider_a2.set_deadband(1, 509, 4); // deadband controls designated range where input movements do not produce output change to avoid jitter.
-  scaling_slider_a2.map(&scalingFilter2D.ratio); // map the value of the slider to the scaling filter ratio.
-
-
+ // -- Configure Wave Generator --
+  wave_gen.frequency = 0;
+  wave_gen.output.map(&channel_z.input_target_position);
 
   // -- Start the stepdance library --
   // This activates the system.
@@ -179,27 +130,27 @@ void loop() {
 }
 
 void pen_down(){
-  position_gen.go(-4, ABSOLUTE, 100);
+  position_gen.go(-200, ABSOLUTE, 2000);
 }
 
 void pen_up(){
-  position_gen.go(4, ABSOLUTE, 100);
+  position_gen.go(200, ABSOLUTE, 2000);
 }
 
-void motors_enable(){
-  enable_drivers();
+void generator_state_1(){
+wave_gen.frequency = 1.0; 
 }
 
-void motors_disable(){
-  disable_drivers();
+void generator_state_2(){
+  wave_gen.frequency = 10.0; 
 }
+
+void generator_state_3(){
+  wave_gen.frequency = 0.0; 
+  
+}
+
 
 void report_overhead(){
-  // Serial.println(channel_z.target_position, 4);
-  // Serial.println(stepdance_get_cpu_usage(), 4);
-  Serial.print(" axidraw X: ");
-  Serial.print(axidraw_kinematics.input_x.read(ABSOLUTE), 4);
-  Serial.print(" axidraw Y: ");
-  Serial.print(axidraw_kinematics.input_y.read(ABSOLUTE), 4);
-  Serial.print("\n");
+  Serial.println(stepdance_get_cpu_usage(), 4);
 }
