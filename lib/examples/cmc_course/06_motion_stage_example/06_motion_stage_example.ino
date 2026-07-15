99736cd5ba8e9e86407a23afe4ba258da48f56a1
em-yu <ermahler.yu@gmail.com>
2026-07-14 17:22:48 -0700

    Change examlpe sketches to help with a demo

--- lib/examples/cmc_course/06_motion_stage_example/06_motion_stage_example.ino
+++ lib/examples/cmc_course/06_motion_stage_example/06_motion_stage_example.ino
@@ -17,18 +17,21 @@ A part of the Mixing Metaphors Project
 
 
 OutputPort output_a;  // motor for motion stage
+OutputPort output_b;  // motor for demo motor
 
 
 // -- Define Motion Channels --
 
 Channel channel_a;  //motion channel for motor
+Channel channel_b;  //motion channel for motor
 
 
 // -- Define Encoders --
 Encoder encoder_1;  // left knob, controls horizontal
 
 // -- Define Generators --
-VelocityGenerator velocityGen;
+VelocityGenerator velocityGenA;
+VelocityGenerator velocityGenB;
 
 // -- RPC Interface --
 RPC rpc;
@@ -36,6 +39,7 @@ RPC rpc;
 void setup() {
   // -- Configure and start the output ports --
   output_a.begin(OUTPUT_A); // "OUTPUT_A" specifies the physical port on the PCB for the output.
+  output_b.begin(OUTPUT_B); // "OUTPUT_A" specifies the physical port on the PCB for the output.
 
 
   // Enable the output drivers
@@ -45,6 +49,7 @@ void setup() {
   channel_a.begin(&output_a, SIGNAL_E); // Connects the channel to the "E" signal on "output_a".
                                         // We choose the "E" signal because it results in a step pulse of 7us,
                                         // which is more than long enough for the driver IC.
+  channel_b.begin(&output_b, SIGNAL_E); 
 
   // Ratio calculation Linear distance = number of rotations x Lead
   // we are using a 2 lead screw = 2 mm per revolution
@@ -54,6 +59,8 @@ void setup() {
                                                 // For the axidraw, 25.4mm == 2874 steps
   channel_a.invert_output();  // CALL THIS TO INVERT THE MOTOR DIRECTION IF NEEDED
 
+  channel_b.set_ratio(1, 40);
+
   // -- Configure and start the encoders --
   encoder_1.begin(ENCODER_1); // "ENCODER_1" specifies the physical port on the PCB
   encoder_1.set_ratio(1, 2400);  // 24mm per revolution, where 1 rev == 2400 encoder pulses
@@ -61,15 +68,20 @@ void setup() {
   encoder_1.invert(); //invert the encoder direction
   encoder_1.output.map(&channel_a.input_target_position);
 
-  velocityGen.begin();
-  velocityGen.output.map(&channel_a.input_target_position);
+  velocityGenA.begin();
+  velocityGenA.output.map(&channel_a.input_target_position);
+
+  velocityGenB.begin();
+  velocityGenB.output.map(&channel_b.input_target_position);
 
   // -- Control interface (RPC) --
   rpc.begin(); 
 
   
-  // Call example: {"name": "set_speed_y", "args": [5]}
-  rpc.enroll("set_speed", set_speed);
+  // Call example: {"name": "set_speed_a", "args": [5]}
+  rpc.enroll("set_speed_a", set_speed_a);
+  // Call example: {"name": "set_speed_b", "args": [5]}
+  rpc.enroll("set_speed_b", set_speed_b);
 
 
 
@@ -88,11 +100,13 @@ void loop() {
 
 
 void report_overhead(){
-  Serial.println(stepdance_get_cpu_usage(), 4);
+  // Serial.println(stepdance_get_cpu_usage(), 4);
 }
 
-void set_speed(float32_t speed){
-  // TODO: modify to set the vertical velocity generator speed
-   velocityGen.speed_units_per_sec = speed;
+void set_speed_a(float32_t speed){
+   velocityGenA.speed_units_per_sec = speed;
+}
 
+void set_speed_b(float32_t speed){
+   velocityGenB.speed_units_per_sec = speed;
 }
