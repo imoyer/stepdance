d36f38a33066477490b32b4a3073541be8ce055a
Ilan Moyer <imoyer@mit.edu>
2026-08-11 14:28:09 -0400

    updated to resolve merge issue

--- lib/interpolators.hpp
+++ lib/interpolators.hpp
@@ -167,6 +167,11 @@ class TimeBasedInterpolator : public Plugin{
      */
     BlockPort output_duration;
 
+    /**
+     * @brief Cancel the current queue.
+     */
+    void reset_block_queue();
+
     volatile uint16_t active_block_id; //stores the current active block
   
     enum{
@@ -192,7 +197,6 @@ class TimeBasedInterpolator : public Plugin{
     volatile uint16_t next_read_index; //next read index in the block queue 
 
     void advance_head(volatile uint16_t* target_head); //handles roll-overs etc
-    void reset_block_queue();
     void pull_block(); //pulls a block from the queue and into the active buffer
     volatile uint8_t in_block = 0; //1 if actively reading a block
 
